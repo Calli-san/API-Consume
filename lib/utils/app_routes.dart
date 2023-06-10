8664537b2016c2c63af025c4d28cede4fe1d5209
Calli-san <fzrzf1231@gmail.com>
2023-06-10 15:11:13 +0700

    Add app routes

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,19 +1,50 @@
+import 'package:api_berita_app/models/post.dart';
+import 'package:api_berita_app/pages/add_post_page.dart';
+import 'package:api_berita_app/pages/home_page.dart';
+import 'package:api_berita_app/pages/post_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRoutes {
   static const home = "home";
+  static const post = "post";
+  static const addPost = "add-post";
 
   static Page _homePageBuilder(BuildContext context, GoRouterState state) {
+    return const MaterialPage(child: HomePage());
+  }
+
+  static Page _postPageBuilder(BuildContext context, GoRouterState state) {
     return MaterialPage(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Home Page'),
-        ),
+      child: PostPage(
+        post: state.extra as Post,
       ),
     );
   }
 
-  
-   
+  static Page _addpostPageBuilder(BuildContext context, GoRouterState state) {
+    return const MaterialPage(child: AddPostPage());
+  }
+
+  static GoRouter goRouter = GoRouter(
+    initialLocation: "/home",
+    routes: [
+      GoRoute(
+          name: home,
+          path: "/home",
+          pageBuilder: _homePageBuilder,
+          routes: [
+            GoRoute(
+              name: post,
+              path: "post",
+              pageBuilder: _postPageBuilder,
+            ),
+            GoRoute(
+              name: addPost,
+              path: "add-post",
+              pageBuilder: _addpostPageBuilder,
+            ),
+          ]),
+    ],
+  );
 }
